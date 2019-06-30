f41e4f17f23bf9b844d3aaf3d221fa4dba5ca00c
Zakaria <zknabdi@gmail.com>
2019-06-29 20:42:54 -0500

    added business logic and retrofit configurations

--- app/src/main/java/com/udacity/popularmoviesapp/model/Movie.java
+++ app/src/main/java/com/udacity/popularmoviesapp/model/Movie.java
@@ -1,4 +1,59 @@
 package com.udacity.popularmoviesapp.model;
 
-public class Movie {
+import com.google.gson.annotations.SerializedName;
+/*
+    Built using JSONConvertToPOJO
+ */
+import java.io.Serializable;
+import java.util.List;
+
+public class Movie implements Serializable {
+    @SerializedName("page")
+    private int page;
+    @SerializedName("total_results")
+    private int totalResults;
+    @SerializedName("total_pages")
+    private int totalPages;
+    @SerializedName("results")
+    private List<Movie> movieList;
+
+    public Movie(int page, int totalResults, int totalPages, List<Movie> movieList) {
+        this.page = page;
+        this.totalResults = totalResults;
+        this.totalPages = totalPages;
+        this.movieList = movieList;
+    }
+
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<Movie> getMovieList() {
+        return movieList;
+    }
+
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
 }
--- app/src/main/java/com/udacity/popularmoviesapp/service/GetMovieDataService.java
+++ app/src/main/java/com/udacity/popularmoviesapp/service/GetMovieDataService.java
@@ -1,4 +1,16 @@
 package com.udacity.popularmoviesapp.service;
 
+import com.udacity.popularmoviesapp.model.MovieResults;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface GetMovieDataService {
+
+    @GET("popular")
+    Call<MovieResults> getMoviesByPopularity(@Query("result") int result,@Query("db_api_key") String db_api_key);
+
+    @GET("top_rated")
+    Call<MovieResults> getMoviesByTopRated(@Query("result") int result,@Query("db_api_key") String db_api_key);
 }
